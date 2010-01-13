e65466d5d28f799367b807c547b83d72c6e478c7
Stas Boukarev <stassats@gmail.com>
2010-01-13 23:42:04 +0300

    http.lisp: add ability to reuse already established connections.

--- http.lisp
+++ http.lisp
@@ -3,18 +3,21 @@
 
 (defparameter *stream* nil)
 
-(defun request (url &optional parameters)
+(defun request (url &key parameters reuse-connection)
   (multiple-value-bind (body code headers uri stream must-close reason-phrase)
       (drakma:http-request url
                            :parameters parameters
                            :additional-headers '(("Accept-Encoding" . "gzip"))
-                           ;; :keep-alive t
-                           ;; :close nil
-                           ;; :stream *stream*
-                           )
-    ;; (setf *stream*
-    ;;       (unless must-close
-    ;;         stream))
+                           :keep-alive reuse-connection
+                           :close (not reuse-connection)
+                           :stream (when (and reuse-connection
+                                              (streamp *stream*)
+                                              (open-stream-p *stream*))
+                                     *stream*))
+    (when reuse-connection
+      (setf *stream*
+            (unless must-close
+              stream)))
     (cond ((/= code 200) (error "http error ~a." code))
           ((equal "gzip" (cdr (assoc :content-encoding headers)))
            (let ((result (chipz:decompress nil 'chipz:gzip body)))
