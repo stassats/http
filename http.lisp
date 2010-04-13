ebdd14738582bb5db365942f0e76b75a6da43ecb
Stas Boukarev <stassats@gmail.com>
2010-04-13 06:51:21 +0400

    http.lisp: Add :binary option.

--- http.lisp
+++ http.lisp
@@ -3,7 +3,21 @@
 
 (defparameter *stream* nil)
 
-(defun request (url &key parameters reuse-connection)
+(defun ungzip (body headers binary)
+  (let ((charset
+         (drakma:parameter-value "charset"
+                                 (nth-value 2
+                                            (drakma:get-content-type headers))))
+        (decompressed (chipz:decompress nil 'chipz:gzip body)))
+    (if binary
+        decompressed
+        (flexi-streams:octets-to-string decompressed
+                                        :external-format
+                                        (intern (string-upcase charset)
+                                                :keyword)))))
+
+
+(defun request (url &key parameters reuse-connection binary)
   (multiple-value-bind (body code headers uri stream must-close reason-phrase)
       (drakma:http-request url
                            :parameters parameters
@@ -13,16 +27,16 @@
                            :stream (when (and reuse-connection
                                               (streamp *stream*)
                                               (open-stream-p *stream*))
-                                     *stream*))
+                                     *stream*)
+                           :force-binary binary)
+    (declare (ignore reason-phrase))
     (when reuse-connection
       (setf *stream*
             (unless must-close
               stream)))
     (values
      (cond ((/= code 200) (error "http error ~a." code))
-           ((equal "gzip" (cdr (assoc :content-encoding headers)))
-            (let ((result (chipz:decompress nil 'chipz:gzip body)))
-              (map-into (make-string (length result))
-                        #'code-char result)))
+           ((equal "gzip" (drakma:header-value :content-encoding headers))
+            (ungzip body headers binary))
            (t body))
      uri)))
