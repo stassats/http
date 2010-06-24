837feda0967ba2bda6896f64077356e82a674a58
Stas Boukarev <stassats@gmail.com>
2010-06-24 13:37:30 +0400

    Move storage into a separate project.

--- packages.lisp
+++ packages.lisp
@@ -1,24 +1,5 @@
 ;;; -*- Mode: Lisp -*-
 
-(defpackage #:storage
-  (:use #:closer-common-lisp)
-  (:shadow #:count #:delete)
-  (:export
-   #:id
-   #:add
-   #:where
-   #:delete
-   #:objects-of-type
-   #:map-type
-
-   #:storable-class
-   #:identifiable
-   #:lookup
-   #:interlink-objects
-   #:load-data
-   #:save-data
-   #:with-storage))
-
 (defpackage #:movies
   (:use #:closer-common-lisp)
   (:export
